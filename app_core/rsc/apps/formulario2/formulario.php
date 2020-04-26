fa555b346911bc18e1de55a3abe2cd39756cdda2
cjosue13 <44554351+cjosue13@users.noreply.github.com>
2020-04-26 17:14:55 -0600

    actualizacion Final

--- app_core/rsc/apps/formulario2/formulario.php
+++ app_core/rsc/apps/formulario2/formulario.php
@@ -42,57 +42,63 @@ if (isset($_POST["btn_eliminar"])) {
     <script type="text/javascript" src="js/funciones1.js"></script>
 </head>
 
-<body>
+<body style="background-color: gray;">
     <?php echo ($message != "") ? "<script> alertify.set('notifier','position', 'top-center'); alertify.success('$message');</script>" : ""; ?>
 
     <h2 id="title">FORMULARIO</h2>
-    <section id="panel_form">
-        <form method="post" id="frm_productos" name="frm_productos" action="formulario.php">
-            <input placeholder="Cédula: " type="text" class="campo_texto" maxlength="9" value="" tabindex="1" id="txt_cedula" name="txt_cedula">
-            <input placeholder="Nombre: " type="text" class="campo_texto" maxlength="32" value="" tabindex="2" id="txt_nombre" name="txt_nombre">
-            <input placeholder="Apellido1: " type="text" class="campo_texto" maxlength="32" value="" tabindex="3" id="txt_apellido1" name="txt_apellido1">
-            <input placeholder="Apellido2: " type="text" class="campo_texto" maxlength="32" value="" tabindex="4" id="txt_apellido2" name="txt_apellido2">
-            <input placeholder="Teléfono: " type="text" class="campo_texto" maxlength="8" value="" tabindex="5" id="txt_telefono" name="txt_telefono">
-            <input placeholder="Email: " type="text" class="campo_texto" maxlength="64" value="" tabindex="6" id="txt_email" name="txt_email">
-            <input placeholder="Dirección: " type="text" class="campo_texto" maxlength="128" value="" tabindex="7" id="txt_direccion" name="txt_direccion">
-            <input placeholder="Departamento: " type="text" class="campo_texto" maxlength="32" value="" tabindex="8" id="txt_departamento" name="txt_departamento">
-            <input placeholder="Puesto: " type="text" class="campo_texto" maxlength="32" value="" tabindex="9" id="txt_puesto" name="txt_puesto">
-            <input placeholder="Salario: " type="text" class="campo_texto" maxlength="11" value="" tabindex="10" id="txt_salario" name="txt_salario" onKeyPress="return validate(event)">
-            <input placeholder="Observaciones: " type="text" class="campo_texto" maxlength="128" value="" tabindex="11" id="txt_observaciones" name="txt_observaciones">
-            <input type="date" value="" tabindex="12" id="txt_fecha" name="txt_fecha">
-            <input type="file" name="txt_foto" class="campo_texto" tabindex="13" id="txt_foto" id="txt_file">
-            <button class="boton" type="button" onclick="publicarAxios()" tabindex="14" name="btn_guardar" id="btn_guardar">Guardar</button>
-            <button class="boton" type="button" onclick="eliminarAxios()" name="btn_eliminarAJAX" id="btn_eliminarAJAX" tabindex="15">Eliminar</button>
-        </form>
-    </section>
-    <section id="panel_data">
-        <form method="post" id="frm_busqueda" name="frm_busqueda">
-            <input type="text" value="" placeholder="Buscar por Nombre o cedula De la persona" size="50" name="txt_busq" id="txt_busq" class="search" tabindex="16" onkeyup="cargarProductos(this.value)">
-        </form>
-        <br><br>
-        <div id="resultados">
-            <table>
-                <thead>
-                    <td>CEDULA</td>
-                    <td>NOMBRE</td>
-                    <td>APELLIDO1</td>
-                    <td>APELLIDO2</td>
-                    <td>TELEFONO</td>
-                    <td>EMAIL</td>
-                    <td>DIRECCION</td>
-                    <td>DEPARTAMENTO</td>
-                    <td>PUESTO</td>
-                    <td>SALARIO</td>
-                    <td>OBSERVACIONES</td>
-                    <td>FOTO</td>
-                    <td>FECHA</td>
-                </thead>
-                <tbody id="grid">
-                    <?php echo obtenerProductos($conexion); ?>
-                </tbody>
-            </table>
-        </div>
-    </section>
+
+    <div style="background-color: gray;">
+        <section id="panel_form">
+            <form method="post" id="frm_productos" name="frm_productos" action="formulario.php">
+                <input placeholder="Cédula: " type="text" class="campo_texto" maxlength="9" value="" tabindex="1" id="txt_cedula" name="txt_cedula">
+                <input placeholder="Nombre: " type="text" class="campo_texto" maxlength="32" value="" tabindex="2" id="txt_nombre" name="txt_nombre">
+                <input placeholder="Apellido1: " type="text" class="campo_texto" maxlength="32" value="" tabindex="3" id="txt_apellido1" name="txt_apellido1">
+                <input placeholder="Apellido2: " type="text" class="campo_texto" maxlength="32" value="" tabindex="4" id="txt_apellido2" name="txt_apellido2">
+                <input placeholder="Teléfono: " type="text" class="campo_texto" maxlength="8" value="" tabindex="5" id="txt_telefono" name="txt_telefono">
+                <input placeholder="Email: " type="text" class="campo_texto" maxlength="64" value="" tabindex="6" id="txt_email" name="txt_email">
+                <input placeholder="Dirección: " type="text" class="campo_texto" maxlength="128" value="" tabindex="7" id="txt_direccion" name="txt_direccion">
+                <input placeholder="Departamento: " type="text" class="campo_texto" maxlength="32" value="" tabindex="8" id="txt_departamento" name="txt_departamento">
+                <input placeholder="Puesto: " type="text" class="campo_texto" maxlength="32" value="" tabindex="9" id="txt_puesto" name="txt_puesto">
+                <input placeholder="Salario: " type="text" class="campo_texto" maxlength="11" value="" tabindex="10" id="txt_salario" name="txt_salario" onKeyPress="return validate(event)">
+                <input placeholder="Observaciones: " type="text" class="campo_texto" maxlength="128" value="" tabindex="11" id="txt_observaciones" name="txt_observaciones">
+                <input type="date" value="" tabindex="12" id="txt_fecha" name="txt_fecha">
+                <input type="file" name="txt_foto" class="campo_texto" tabindex="13" id="txt_foto" id="txt_file">
+                <button class="boton" type="button" onclick="publicarAxios()" tabindex="14" name="btn_guardar" id="btn_guardar">Guardar</button>
+                <button class="boton" type="button" onclick="eliminarAxios()" name="btn_eliminarAJAX" id="btn_eliminarAJAX" tabindex="15">Eliminar</button>
+            </form>
+        </section>
+        <section id="panel_data">
+            <form method="post" id="frm_busqueda" name="frm_busqueda">
+                <input type="text" value="" placeholder="Buscar por Nombre o cedula De la persona" size="50" name="txt_busq" id="txt_busq" class="search" tabindex="16" onkeyup="cargarProductos(this.value)">
+            </form>
+            <br><br>
+            <div id="resultados">
+                <table>
+                    <thead>
+                        <td>CEDULA</td>
+                        <td>NOMBRE</td>
+                        <td>APELLIDO1</td>
+                        <td>APELLIDO2</td>
+                        <td>TELEFONO</td>
+                        <td>EMAIL</td>
+                        <td>DIRECCION</td>
+                        <td>DEPARTAMENTO</td>
+                        <td>PUESTO</td>
+                        <td>SALARIO</td>
+                        <td>OBSERVACIONES</td>
+                        <td>FOTO</td>
+                        <td>FECHA</td>
+                    </thead>
+                    <tbody id="grid">
+                        <?php echo obtenerProductos($conexion); ?>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    </div>
+
+
+    
 </body>
 
 </html>
